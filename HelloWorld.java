89855a9f687c67e13b6aa35f9325ad44b1ac14cd
StefanoScolari <stefano.scolari@studio.unibo.it>
2020-11-02 11:19:26 +0100

    Now really added .java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,17 +3,15 @@ public final class HelloWorld {
 	private static final String AUTHOR = "Danilo Pianini";
 
 	public static void main(final String[] args) {
-<<<<<<< HEAD
 		System.out.println("This program is running in a PC with " + procNumber() + " logic processors!");
 		System.out.println(printName());
 	}
 
 	public static int procNumber() {
 		return Runtime.getRuntime().availableProcessors();
-=======}
-	public static void printName(){
-		System.out.println("This program has been realised by " + AUTHOR);
->>>>>>> feature
+	}
+	public static String printName(){
+		return "This program has been realised by " + AUTHOR;
 	}
 
 }
